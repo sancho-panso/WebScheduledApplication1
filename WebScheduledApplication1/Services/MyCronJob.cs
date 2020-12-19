fc9ba1ffd36adf179cf3377cc40b40f4fc5ba68f
Michail Gimpelson <sancho.panso@gmail.com>
2020-12-19 12:56:33 +0200

    Add project files.

---
+++ WebScheduledApplication1/Services/MyCronJob.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using WebScheduledApplication1.Controllers;
+
+namespace WebScheduledApplication1.Services
+{
+    public class MyCronJob : CronJobService
+    {
+        private readonly ILogger<MyCronJob> _logger;
+
+        public MyCronJob(IScheduleConfig<MyCronJob> config, ILogger<MyCronJob> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+             _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob  is working.");
+            
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
---
+++ WebScheduledApplication1/Services/MyScopedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebScheduledApplication1.Services
+{
+    public interface IMyScopedService
+    {
+        Task DoWork(CancellationToken cancellationToken);
+    }
+
+    public class MyScopedService : IMyScopedService
+    {
+        private readonly ILogger<MyScopedService> _logger;
+
+        public MyScopedService(ILogger<MyScopedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} MyScopedService is working.");
+            await Task.Delay(1000 * 20, cancellationToken);
+        }
+    }
+}
